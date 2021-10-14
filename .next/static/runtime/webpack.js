f6a6320233b8de66823e1a1d249301d6ef24abd9
Willian Silva <willian@wiki4fit.com.br>
2021-10-14 01:51:42 -0300

    feat: menu

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a5e72ff40331b4415790";
+/******/ 	var hotCurrentHash = "4a966c315b8cf8585c33";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
