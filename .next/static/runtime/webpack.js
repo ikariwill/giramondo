a06c904aae61f616e32ef5cc2ccdac71df09e550
Willian Silva <willian@wiki4fit.com.br>
2021-11-23 02:42:50 -0300

    git ignore

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "844509c698ba7858c95f";
+/******/ 	var hotCurrentHash = "9e41d30a1476ec67f7c9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
