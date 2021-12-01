d673e503245ee9473eb62e0f104925d2888f3544
Willian Silva <willian@wiki4fit.com.br>
2021-12-01 18:08:57 -0300

    update

--- src/sections/landing1/Services.js
+++ src/sections/landing1/Services.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { useRouter } from 'next/router'
 import { Container, Row, Col } from "react-bootstrap";
 
 const Services = () => {
-  const router = useRouter()
 
   return (
     <>
